2fa6f53ce6b439e2655de78ba98897e2e6ef5387
abdul-nut <suryayasaantariksa@gmail.com>
2024-09-28 22:08:09 +0800

    error(belum selesai)

--- nextjs-starterpack-master/app/page.tsx
+++ nextjs-starterpack-master/app/page.tsx
@@ -1,61 +1,83 @@
 "use client";
 
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-
-import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/atoms/Icons";
+import { Formik } from "formik";
+import React from "react";
+import { Input } from "@nextui-org/input";
+import { Button } from "@nextui-org/button";
+import { useMutation } from "@tanstack/react-query";
+import axiosInstance from "@/lib/axios";
+import { toast } from "react-toastify";
 
 export default function Home() {
-  {/* Kita ngoding di dalam sini */}
+  const [url, setUrl] = React.useState('');
+
+  const formMutation = useMutation({
+    mutationFn: (body: { url: string, slug: string }) => {
+      return axiosInstance.post('/short-link', body);
+    },
+    onSuccess: (data) => {
+      toast.success('Short link created');
+      setUrl(data.data.url)
+    },
+    onError: (error: any) => {
+      if (error.status === 400) {
+        toast.error('Invalid URL or Slug');
+      }
+
+      if (error.status === 500) {
+        toast.error('Internal Server Error');
+      }
+    },
+  })
+
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center gap-2">
       <div className="inline-block max-w-xl text-center justify-center">
-        <h1 className={title()}>Mari kita&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>Ngoding&nbsp;</h1>
-        <br />
-        <h1 className={title()}>menggunakan </h1>
-        <h1 className={title({ color: "blue" })}>Next.js </h1>
-        <h1 className={title()}>dan </h1>
-        <h1 className={title({ color: "cyan" })}>NextUI</h1>
-        <h2 className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </h2>
+        <h1 className={title()}>Website </h1>
+        <h1 className={title({ color: 'cyan'})}>URLShortly</h1>
+        <h2 className={subtitle()}>Shorten URL easily</h2>
       </div>
 
-      <div className="flex gap-3 justify-center">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
+      {
+        url != '' && (
+          <div className={"flex flex-row gap-2"}>
+            <Input value={url} />
+            <Button color={"primary"} onClick={() => {
+              navigator.clipboard.writeText(url);
+              toast.success('Link copied');
+            }}>Copy</Button>
+          </div>
+        )
+      }
 
-      <div className="flex justify-center mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
+      <Formik initialValues={{
+        url: "",
+        slug: ""
+      }} onSubmit={(values) => {
+        formMutation.mutate(values);
+        setUrl('')
+      }}>
+        {({ values, handleSubmit, handleChange }) => (
+          <form className={"flex flex-col gap-2"} onSubmit={handleSubmit}>
+            <Input
+              label="URL"
+              placeholder="Enter your URL"
+              name={"url"}
+              value={values.url}
+              onChange={handleChange}
+            />
+            <Input
+              label="Slug"
+              placeholder="Enter your slug ex: shortLink"
+              name={"slug"}
+              value={values.slug}
+              onChange={handleChange}
+            />
+            <Button type={"submit"} color={'primary'} isLoading={formMutation.isPending}>Create</Button>
+          </form>
+        )}
+      </Formik>
     </div>
   );
-  {/* Kita ngoding di dalam sini */}
 }
